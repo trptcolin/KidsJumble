e3e55c20c48f6be5b85973dc6c4b1be187d98b19
Colin Jones <trptcolin@gmail.com>
2018-01-21 10:43:08 -0600

    Spike out an MVP with Owen

--- KidsJumble/ViewController.swift
+++ KidsJumble/ViewController.swift
@@ -1,18 +1,65 @@
-//
-//  ViewController.swift
-//  KidsJumble
-//
-//  Created by Colin Jones on 1/21/18.
-//  Copyright © 2018 Colin Jones. All rights reserved.
-//
-
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var choicesStackView: UIStackView!
+
+    @IBOutlet weak var answersStackView: UIStackView!
+
+    @IBAction func playAgain(_ sender: Any) {
+        startGame(answer: pickRandomWord())
+    }
+
+    let possibleAnswers = [
+        "MOMMY",
+        "OWEN",
+        "HENRY",
+        "NOTHING",
+        "NO",
+        "ELSE"
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startGame(answer: pickRandomWord())
+    }
+    
+    private func pickRandomWord() -> String {
+        let total = possibleAnswers.count
+        let randomIndex = Int(arc4random()) % total
+        return possibleAnswers[randomIndex]
+    }
+
+    private func startGame(answer: String) {
+        choicesStackView.subviews.forEach { (view) in
+            view.removeFromSuperview()
+        }
+        answersStackView.subviews.forEach { (view) in
+            view.removeFromSuperview()
+        }
+
+        let choices = Array(answer)
+        let randomizedChoices = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: choices)
+
+        for letter in randomizedChoices {
+            let button = makeButton(text: String(describing: letter))
+            choicesStackView.addArrangedSubview(button)
+        }
+    }
+
+    private func makeButton(text: String) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = UIColor.green
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        return button
+    }
+
+    @objc func buttonAction(sender: UIButton) {
+        let otherSuperview = (sender.superview == choicesStackView) ? answersStackView : choicesStackView
+        sender.removeFromSuperview()
+        otherSuperview!.addArrangedSubview(sender)
     }
 
     override func didReceiveMemoryWarning() {
